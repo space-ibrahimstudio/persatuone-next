40e67fa8bb7678f0443ab3904419ec540f9443e2
Maulana Malik Ibrahim <ibrahimcpersonal@gmail.com>
2024-03-02 20:51:51 +0700

    feat: add a sendMail function in contact section using nodemailer

---
+++ pages/api/sendEmail.js
@@ -0,0 +1,34 @@
+import nodemailer from "nodemailer";
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const { name, email, subject, message } = req.body;
+
+    const transporter = nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      secure: true,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+
+    try {
+      await transporter.sendMail({
+        from: email,
+        to: "sales@persatu.one",
+        subject: subject,
+        text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+      });
+
+      res.status(200).json({ success: true });
+    } catch (error) {
+      console.error("Error:", error);
+      res.status(500).json({ success: false, error: "Failed to send email." });
+    }
+  } else {
+    res.setHeader("Allow", ["POST"]);
+    res.status(405).json({ error: `Method ${req.method} not allowed` });
+  }
+}
