602c21545e997e5b012fac2a4127a00430124bd5
Maulana Malik Ibrahim <ibrahimcpersonal@gmail.com>
2024-04-18 14:10:22 +0700

    feat: add sitemap xml

---
+++ pages/sitemap.xml.js
@@ -0,0 +1,37 @@
+import { fetchBlogList } from "@/utils/data";
+
+const Sitemap = () => {};
+
+export const getServerSideProps = async ({ res }) => {
+  const response = await fetchBlogList();
+  const posts = response.data;
+
+  if (!posts) {
+    res.setHeader("Content-Type", "text/xml");
+    res.write(`<?xml version="1.0" encoding="UTF-8"?>`);
+    res.end();
+    return { props: {} };
+  }
+
+  const baseUrl = "https://persatu.one";
+
+  let sitemapXml = `<?xml version="1.0" encoding="UTF-8"?>`;
+  sitemapXml += `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`;
+
+  sitemapXml += `<url><loc>${baseUrl}</loc><changefreq>daily</changefreq><priority>1</priority><lastmod>${new Date().toISOString()}</lastmod></url>`;
+  sitemapXml += `<url><loc>${baseUrl}/news</loc><changefreq>weekly</changefreq><priority>0.8</priority><lastmod>${new Date().toISOString()}</lastmod></url>`;
+
+  posts.forEach((post) => {
+    sitemapXml += `<url><loc>${baseUrl}/news/${post.slug}</loc><lastmod>${post.blogcreate}</lastmod></url>`;
+  });
+
+  sitemapXml += `</urlset>`;
+
+  res.setHeader("Content-Type", "text/xml");
+  res.write(sitemapXml);
+  res.end();
+
+  return { props: {} };
+};
+
+export default Sitemap;
